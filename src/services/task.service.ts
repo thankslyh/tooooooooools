046a7bf8c1d367455cb1309dac50f0b9506976b1
thankslyh <liyinghao@shuidi-inc.com>
2024-06-07 16:24:56 +0800

    feat: 掘金定时签到

--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
+import sign from 'src/tools/juejin/sign';
 import { submitOrder } from 'src/tools/pdl/server';
 import { readConfig } from 'src/tools/pdl/util';
 
@@ -7,17 +8,23 @@ import { readConfig } from 'src/tools/pdl/util';
 export class TasksService {
   private readonly logger = new Logger(TasksService.name);
 
+  // @Cron('0 30 09 * * *')
+  // async handleCron() {
+  //   const config = await readConfig();
+  //   for (const i in config) {
+  //     const cnf = config[i];
+  //     if (cnf) {
+  //       submitOrder(cnf).then(() => {
+  //         this.logger.debug(`${i} 下单成功，请去支付`);
+  //       });
+  //     }
+  //   }
+  //   this.logger.debug('Called when the current second is 1');
+  // }
+
   @Cron('0 30 09 * * *')
   async handleCron() {
-    const config = await readConfig();
-    for (const i in config) {
-      const cnf = config[i];
-      if (cnf) {
-        submitOrder(cnf).then(() => {
-          this.logger.debug(`${i} 下单成功，请去支付`);
-        });
-      }
-    }
-    this.logger.debug('Called when the current second is 1');
+    const res = await sign();
+    this.logger.debug(JSON.stringify(res.data));
   }
 }
