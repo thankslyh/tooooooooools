df93a3cb0dcfa21409b469aabdc521209703ff39
thankslyh <liyinghao@shuidi-inc.com>
2024-06-13 11:22:24 +0800

    feat: 完善抽奖领取流程

--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
+import draw from 'src/tools/juejin/draw';
+import receive from 'src/tools/juejin/receive';
 import sign from 'src/tools/juejin/sign';
 // import { submitOrder } from 'src/tools/pdl/server';
 // import { readConfig } from 'src/tools/pdl/util';
@@ -23,14 +25,25 @@ export class TasksService {
   //   this.logger.debug('Called when the current second is 1');
   // }
 
-  @Cron('0 27 09 * * *')
+  @Cron('04 11 * * *')
   async handleCron() {
     this.logger.debug('执行定时任务...........');
-    const res = await sign();
-    fangtangPusher(
-      `掘金签到：${+res.data.err_no === 0 ? '成功' : '失败'}`,
-      res.data.err_msg,
-    );
-    this.logger.debug(JSON.stringify(res.data));
+    try {
+      let text = '';
+      const res = await sign();
+      text += `签到结果：${res.data.err_msg}\n\n`;
+      const drawRes = await draw();
+      text += `抽奖结果： ${JSON.stringify(drawRes.data)}\n\n`;
+      const receiveRes = await receive();
+      text += `领取结果： ${JSON.stringify(receiveRes.data)}\n\n`;
+      fangtangPusher(
+        `掘金签到：${+res.data.err_no === 0 ? '成功' : '失败'}`,
+        text,
+      );
+      this.logger.debug('执行成功');
+    } catch (e) {
+      fangtangPusher(`掘金签到：错误`, JSON.stringify(e));
+      this.logger.error(e);
+    }
   }
 }
